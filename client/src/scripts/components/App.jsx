3e5daceb9acf8b91f569af21a7db2b60c531d43b
RomanLevak <RomaLevak@gmail.com>
2019-10-30 17:57:31 +0200

    added login&singup routes

--- client/src/scripts/components/App.jsx
+++ client/src/scripts/components/App.jsx
@@ -4,6 +4,7 @@ import Search from './Search'
 import HomeList from './HomeList'
 import Movies from './routes/Movies'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Form from './Form'
 import '../../styles/index.sass'
 
 class App extends Component {
@@ -15,6 +16,8 @@ class App extends Component {
                     <main className='content-box full-height'>
                         <Search />
                         <Switch>
+                            <Route path='/login' component = {() => <Form />}/>
+                            <Route path='/sing-up' component = {() => <Form />}/>
                             <Route path = '/movies' component = {() => <Movies />} />
                             <Route path='/' component={() => <HomeList/>} />
                         </Switch>
--- client/src/scripts/components/Header.jsx
+++ client/src/scripts/components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
             </div>
             <h1 className='header__title'> <Link to = '/'> movieview</Link></h1>
             <div className='header__buttons'>
-                <button className='btn-border header__btn btn-login'>Login</button>
-                <button className='btn-border header__btn btn-singup'>sing up</button>
+                <Link to='/login' className='btn-border header__btn btn-login'>Login</Link>
+                <Link to='/sing-up' className='btn-border header__btn btn-singup'>sing up</Link>
             </div>
         </header>
     )
