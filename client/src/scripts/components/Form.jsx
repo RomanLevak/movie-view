8480ab94ed95c08dd251b0e1f1dc2b297bdb4d48
RomanLevak <RomaLevak@gmail.com>
2019-10-31 17:47:45 +0200

    add state to a Form

--- client/src/scripts/components/Form.jsx
+++ client/src/scripts/components/Form.jsx
@@ -1,15 +1,30 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleEmailChange = e =>
+        this.setState({email: e.target.value})
+
+    handlePasswordChange = e =>
+        this.setState({password: e.target.value})
+
     render() {
         return (
-            <form className='form form-box'>
+            <form className='form form-box'
+                onSubmit={this.handleSubmit}
+            >
                 <fieldset className='form__field-box'>
                     <fieldset className='form__field'>
                         <label htmlFor="user_email">Email</label>
                         <input className='form__field form__text-input'
                             id='user_email'
                             type="email"
+                            value={this.state.email}
+                            onChange={this.handleEmailChange}
                         />
                     </fieldset>
                     <fieldset className='form__field'>
@@ -17,6 +32,8 @@ class Form extends Component {
                         <input className='form__text-input'
                             id='user_password'
                             type="password"
+                            value={this.state.password}
+                            onChange={this.handlePasswordChange}
                         />
                     </fieldset>
                     <div className="form__buttons-wrap">
