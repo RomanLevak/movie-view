292eb1355d4b30bb67b8f65a253488c7f9d59868
RomanLevak <RomaLevak@gmail.com>
2019-11-06 15:05:36 +0200

    added password encryption

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('../libs/mongoose')
+const crypto = require('crypto')
+const config = require('config')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -23,5 +25,32 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 }, {timestamps: true})
+
+function generatePassword(salt, password) {
+    return new Promise((resolve, reject) => {
+        crypto.pbkdf2(
+            password, salt,
+            config.get('crypto.hash.iterations'), config.get('crypto.hash.length'),
+            'sha512',
+            (err, key) => {
+                if(err) return reject(err)
+
+                resolve(key.toString('hex'))
+            }
+        )
+    })
+}
+
+userSchema.methods.setPassword = async function(password) {
+    this.salt = crypto.randomBytes(config.get('crypto.hash.length')).toString('hex')
+    this.passwordHash = await generatePassword(this.salt, password)
+}
+
+userSchema.methods.checkPassword = async function(password) {
+    if(!password) return false
+
+    const hash = await generatePassword(this.salt, password)
+    return hash === this.passwordHash
+}
 
 module.exports = mongoose.model('User', userSchema)
