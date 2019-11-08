ca1469c2e98bd516d93c00eaa0695ef4d06adaad
RomanLevak <RomaLevak@gmail.com>
2019-11-08 16:23:50 +0200

    add api middleware

---
+++ client/src/scripts/middleware/api.js
@@ -0,0 +1,41 @@
+import {START, SUCCESS, FAIL} from '../constants'
+
+export default () => next => action => {
+    const {callAPI, type, ...rest} = action
+
+    if(!callAPI) return next(action)
+    const {method} = callAPI
+
+    next({
+        ...rest, type: type + START
+    })
+
+    fetch(callAPI.url, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: method || 'GET',
+        body: method == 'POST' ? callAPI.body : null
+    })
+        .then(response => {
+            if(response.status >= 400 && !/^\/?server/.test(callAPI.url))
+                throw new Error(response.statusText)
+
+            return response.json()
+        })
+        .then(response => {
+            if(response.status >= 400)
+                throw new Error(response.message)
+
+            next({
+                ...rest,
+                type: type + SUCCESS,
+                payload: response
+            })
+        })
+        .catch(error => next({
+            ...rest,
+            type: type + FAIL,
+            payload: error.message
+        }))
+}
