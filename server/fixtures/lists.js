42fee52c2265a8cb889bad9cd4768a27d7d8fb46
RomanLevak <RomaLevak@gmail.com>
2019-11-15 13:23:59 +0200

    add new items

--- server/fixtures/lists.js
+++ server/fixtures/lists.js
@@ -1,9 +1,25 @@
 module.exports = [{
     userEmail: 'u1@gmail.com',
-    title: 'top Science fiction movies',
-    movies: ['27205', '603', '157336', '1124']
+    title: 'top Sci-Fi movies',
+    movies: ['27205', '603', '157336', '1124', '280', '10681']
+}, {
+    userEmail: 'u1@gmail.com',
+    title: 'some marvel movies',
+    movies: ['299536', '315635', '1726', '284054', '118340', '284053']
+}, {
+    userEmail: 'u1@gmail.com',
+    title: '10 Best movies from 2018',
+    movies: ['353081', '383498', '424694', '333339', '384680', '500664', '399174', '359940', '299536', '324857']
 }, {
     userEmail: 'u2@gmail.com',
-    title: 'some marvel movies',
-    movies: ['299536', '315635', '1726']
+    title: 'Top IMDB',
+    movies: ['278', '238', '155', '389', '424', '122', '680', '429', '550', '13']
+}, {
+    userEmail: 'u2@gmail.com',
+    title: 'Horror films',
+    movies: ['694', '348', '1091', '13310', '49797', '419430']
+}, {
+    userEmail: 'u2@gmail.com',
+    title: '10 Movies You May Have Skipped in 2018',
+    movies: ['467632', '433310', '455980', '497713', '523777', '446021', '369972', '399404', '389015', '332562']
 }]
