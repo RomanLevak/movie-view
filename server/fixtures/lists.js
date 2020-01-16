53e829a60205437a4e5a68529a83ec271d9aa43f
RomanLevak <RomaLevak@gmail.com>
2020-01-16 13:05:42 +0200

    updated fixtures

--- server/fixtures/lists.js
+++ server/fixtures/lists.js
@@ -1,25 +1,50 @@
 module.exports = [{
     userEmail: 'u1@gmail.com',
-    title: 'top Sci-Fi movies',
-    movies: ['27205', '603', '157336', '1124', '280', '10681']
-}, {
-    userEmail: 'u1@gmail.com',
-    title: 'some marvel movies',
+    title: 'Best marvel movies',
     movies: ['299536', '315635', '1726', '284054', '118340', '284053']
 }, {
     userEmail: 'u1@gmail.com',
     title: '10 Best movies from 2018',
     movies: ['353081', '383498', '424694', '333339', '384680', '500664', '399174', '359940', '299536', '324857']
 }, {
+    userEmail: 'u1@gmail.com',
+    title: '10 Movies You May Have Skipped in 2016',
+    movies: ['332567', '339527', '303858', '401861', '333371', '290250', '319888', '302946', '318846', '329865']
+
+}, {
+    userEmail: 'u1@gmail.com',
+    title: 'Top Sci-Fi movies',
+    movies: ['27205', '603', '157336', '1124', '280', '10681']
+}, {
+    userEmail: 'u1@gmail.com',
+    title: 'top rated Fantasy',
+    movies: ['122', '1891', '129', '497', '120', '10193']
+},{
     userEmail: 'u2@gmail.com',
     title: 'Top IMDB',
     movies: ['278', '238', '155', '389', '424', '122', '680', '429', '550', '13']
 }, {
+    userEmail: 'u1@gmail.com',
+    title: 'Top movies from The Black List',
+    movies: ['106646', '1372', '281957', '186', '45269', '1402', '16869', '68718', '244786', '1124']
+}, {
     userEmail: 'u2@gmail.com',
     title: 'Horror films',
     movies: ['694', '348', '1091', '13310', '49797', '419430']
+},{
+    userEmail: 'u2@gmail.com',
+    title: 'The best crime movies',
+    movies: ['238', '155', '680', '769', '598', '1422', '807', '274', '629', '101', '534780', '311', '107', '26900']
 }, {
     userEmail: 'u2@gmail.com',
     title: '10 Movies You May Have Skipped in 2018',
     movies: ['467632', '433310', '455980', '497713', '523777', '446021', '369972', '399404', '389015', '332562']
+}, {
+    userEmail: 'u2@gmail.com',
+    title: '10 Movies You May Have Skipped in 2017',
+    movies: ['369300', '353070', '457847', '68730', '379019', '337170', '419430', '400928', '339877', '316029']
+}, {
+    userEmail: 'u2@gmail.com',
+    title: 'High rated comedy',
+    movies: ['496243', '637', '77338', '105', '20453', '10193', '194', '107', '359940']
 }]
